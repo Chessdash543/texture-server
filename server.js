04779d116860df8a8c42c576fdaaaa1c30e85209
ChessDash543 <chessdash9@gmail.com>
2025-11-27 19:09:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs");
 const path = require("path");
-const multer = require("multer");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
